b4798ebdb605082b94cc0efdd1c2c32df6470015
Yarin Ovadia <yarin.ovadia@post.runi.ac.il>
2024-02-22 16:54:09 +0200

    update

--- StringOps.java
+++ StringOps.java
@@ -22,13 +22,46 @@ public class StringOps {
     //////                                               ///////
     ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
-        
+        /*
+        String str = "deMO demo DEMO dEMo vowels are fun";
+        String check= "Hello World";
+        System.out.println(capVowelsLowRest(str));
+System.out.println(capVowelsLowRest(check));
+
+*/
+
+
+    }
+    
+    
+    public static String capVowelsLowRest (String string) 
+    {
+        String str = "";
+        for (int i = 0; i < string.length(); i++) {
+            char ch = string.charAt(i);
+            if (ch >= 'a' && ch <= 'z') {
+                if (ch == 'a' || ch == 'e' || ch == 'o' || ch == 'i' || ch == 'u') {
+                    str += (char) (ch - 32);
+                } else {
+                    str += ch;
+                }
+            } else if (ch >= 'A' && ch <= 'Z') {
+                if (ch == 'A' || ch == 'E' || ch == 'O' || ch == 'I' || ch == 'U') {
+                    str += ch;
+                } else {
+                    str += (char) (ch + 32);
+                }
+            }
+            if (ch == ' ') {
+                str += ch;
+            }
+        }
+        return str;
     }
 
-    public static String capVowelsLowRest (String string) {
-        // Write your code here:
-        return "";
-    }
+
+
+
 
     public static String camelCase (String string) {
         // Write your code here:
